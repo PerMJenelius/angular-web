bdd5c9f87fd00dd9e078859264ef1d7185d90504
Per Jenelius <per.jenelius@connection.se>
2017-10-13 14:11:31 +0200

    please?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Content } from './content/content.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    Content
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
