93eb0e28cee8c0621d410ac6bcddc52c5f8460df
Per Jenelius <per.jenelius@connection.se>
2017-10-19 08:37:31 +0200

    ny home-sida

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { ContentService } from "./content.service";
 
 import { IMainpage } from "./mainpage";
@@ -10,29 +10,8 @@ import { IMainpage } from "./mainpage";
 })
 export class ContentComponent implements OnInit {
 
-    activeItem: number = 10;
-    activeHeader: string;
-    activeText: string;
-	subpages: any[];
+    @Input() contentHeader: string;
+    @Input() contentText: string;
 
-	ngOnInit(): void {
-        
-	}
-	
-    showItem(itemNr: number): void {
-
-        //hämta ut rätt text
-        var index = itemNr - 10;
-        this.activeHeader = this.subpages[index].pageName;
-        this.activeText = this.subpages[index].text;
-
-        //styling av navigationen
-        var oldTab = document.getElementById(this.activeItem.toString());
-        var subTab = document.getElementById(itemNr.toString());
-        oldTab.style.setProperty('background-color', '');
-        oldTab.style.setProperty('color', '');
-        subTab.style.setProperty('background-color','rgba(0,0,0,0.33)');
-        subTab.style.setProperty('color','rgba(240,240,240,1)');
-        this.activeItem = itemNr;
-    }
+	ngOnInit(): void { }
 }
